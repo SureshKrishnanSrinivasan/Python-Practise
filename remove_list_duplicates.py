5c9a770ed4e090b0b84ac284f0e83a021ff21fb1
SureshKrishnanSrinivasan <surlicbe@gmail.com>
2018-06-15 16:15:39 +0200

    Sets

---
+++ remove_list_duplicates.py
@@ -0,0 +1,10 @@
+import re
+# sets are unordered
+# sets do not contain repeating values
+
+lst = ["One","Four","Two","Three","Four","Five","Two","Six"]
+lst = set(lst)
+lst = list(lst)
+print(lst)
+
+
